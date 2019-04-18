b3c41d9078493a4a09e309b9d985047fcf22a355
Edgar Basto <edgar.pt@gmail.com>
2019-04-18 12:12:33 +0100

    import data iterating over dataframe

--- spark/tmp/import_data.py
+++ spark/tmp/import_data.py
@@ -2,7 +2,24 @@ import pandas as pd
 
 df = pd.read_excel('../files/base.xlsx', index_col=None, header=None, dtype={'period':str, 'consumption':int, 'price':float})
 year = df[1][1]
+df.columns = ['Descritivo', 'Consumo', 'Preco', 'Total']
 df.dropna(axis=0, thresh=1, inplace=True)
 
+mes=0
+
+for row in df.itertuples(index=True, name='Teste'):
+    meses = ['JANEIRO', 'FEVEREIRO', 'MARÇO', 'ABRIL', 'MAIO', 'JUNHO', 'JULHO', 'AGOSTO', 'SETEMBRO', 'OUTUBRO', 'NOVEMBRO', 'DEZEMBRO']
+    periodo = ['Ponta', 'Cheio', 'Vazio', 'Super Vazio']
+    try:
+        meses.index(getattr(row, 'Descritivo'))
+        mes = meses.index(getattr(row, 'Descritivo')) + 1
+        #print('{} and {}'.format(mes, getattr(row+1, 'Descritivo')))
+    except ValueError:
+        False
+         
+
+    print('{} and {}'.format(getattr(row, 'Descritivo'), getattr(row, 'Consumo')))
+
+
+
 df[1]
-sleep(10)
